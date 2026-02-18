14d1be43efbb43559e0438ed2cffe895d2f0c519
Artem <ArtemKorolev24092009@yandex.ru>
2026-02-18 16:30:10 +0300

    little refactoring

--- source/base.h
+++ source/base.h
@@ -2,9 +2,11 @@
 
 #include <stdlib.h>
 
-typedef unsigned char byte_t;
-typedef unsigned short word_t;
+typedef uint8_t byte_t;
+typedef uint16_t word_t;
 
 #define BYTE_MAX UINT8_MAX
-#define MEMORY_SIZE 65536
 #define WORD_MAX UINT16_MAX
+
+#define KB_64 65536
+#define MEMORY_SIZE KB_64
